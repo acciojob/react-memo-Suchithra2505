dbeba11166f9540a8df329d8da903e0533b23581
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-04-26 05:13:04 +0000

    final commit

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -10,10 +10,10 @@ const UseMemo = () => {
   return (
     <div id="calc">
       <h2>UseMemo Example</h2>
-      <p>Counter: {count}</p>
+      <p id="incr-cnt">Counter: {count}</p>
       <p>{isEven ? '1000000000' : 'Odd'}</p>
       
-      <button onClick={() => setCount(count + 1)} id="incr-cnt">+<b>{count}</b></button>
+      <button onClick={() => setCount(count + 1)} id="incr-btn">+<b>{count}</b></button>
     </div>
   );
 };
