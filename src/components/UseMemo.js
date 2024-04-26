ed96c7b0ff42cc69ea6a98d95be5cef733366c70
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2024-04-26 05:31:16 +0000

    final commit

--- src/components/UseMemo.js
+++ src/components/UseMemo.js
@@ -3,15 +3,17 @@ import React, { useState, useMemo } from 'react';
 const UseMemo = () => {
   const [count, setCount] = useState(0);
 
-  const isEven = useMemo(() => {
-    return count % 2 === 0;
+  // const isEven = useMemo(() => {
+  //   return count % 2 === 0;
+  // }, [count]);
+  const dynamicContent = useMemo(() => {
+    return 1000000000 + count;
   }, [count]);
-
   return (
     <div id="calc">
       <h2>UseMemo Example</h2>
       <p id="incr-cnt">Counter: {count}</p>
-      <p>{isEven ? '1000000000' : '1000000001'}</p>
+      <p>{<p>{dynamicContent}</p>}</p>
       
       <button onClick={() => setCount(count + 1)} id="incr-btn">+<b>{count}</b></button>
     </div>
